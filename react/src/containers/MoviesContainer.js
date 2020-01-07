8535c6583b7c7c49958b104e48bc231f8ff9f5a6
Stephen Fang <safang99@gmail.com>
2020-01-07 13:30:14 -0500

    removed final code

--- react/src/containers/MoviesContainer.js
+++ react/src/containers/MoviesContainer.js
@@ -1,11 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import MoviesIndex from "../components/MoviesIndex"
-import MovieForm from "./MovieForm"
-
 const MoviesContainer = (props) => {
-
-  const [movies, setMovies] = useState([])
 
   useEffect(() => {
     fetch("/api/v1/movies")
@@ -13,35 +8,13 @@ const MoviesContainer = (props) => {
       return response.json()
     })
     .then((moviesBody) => {
-      setMovies(moviesBody.movies)
+      //set state
     })
   }, [])
 
-  const persistNewMovie = (formInfo) => {
-    fetch("/api/v1/movies", {
-      method: "POST",
-      body: JSON.stringify(formInfo)
-    })
-    .then((response) => {
-      return response.json()
-    })
-    .then((persistedMovie) => {
-      // setMovies(movies.concat(persistedMovie))
-      setMovies([...movies, persistedMovie])
-    })
-  }
-
   return(
     <div className="app">
-      <h1> Disney, Disney, Disney (Movies)</h1>
-
-      <MovieForm
-        persistNewMovie={persistNewMovie}
-      />
-
-      <MoviesIndex
-        movies={movies}
-      />
+      <h1> All them Disney Movies</h1>
     </div>
   )
 }
